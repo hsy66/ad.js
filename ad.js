43de1ccf8cf3ac8981206abff68eafbfb766362f
hsy66 <qq353324582@163.com>
2017-03-16 22:31:50 +0800

    Update ad.js

--- ad.js
+++ ad.js
@@ -3859,3 +3859,5 @@ function FindProxyForURL(url, host){
 *     为“FindUserProxyForURL“后粘贴在下方即可。  *
 *                                             *
 **********************************************/
+
+
